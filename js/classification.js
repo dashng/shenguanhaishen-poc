da597bbe7a20b2b5bbc3380c1e56b9299023dfd1
Dai Shaoshuai <shdai@cisco.com>
2015-08-12 07:36:15 +0800

    。。

--- js/classification.js
+++ js/classification.js
@@ -18,8 +18,8 @@ require(['jquery', 'main'], function($){
   					var list = $tr.find('.p_list');
   					list.filter(':not(.p'+index+'_list)').hide();
   					list.filter('.p'+index+'_list').toggle();
-  					$tr.find('i').not($a.find('i')).removeClass('fa-arrow-circle-down fa-arrow-circle-up').addClass('fa-arrow-circle-up');
-  					$a.find('i').toggleClass('fa-arrow-circle-down').toggleClass('fa-arrow-circle-up');
+  					$tr.find('i').not($a.find('i')).removeClass('fa-chevron-circle-down fa-chevron-circle-up').addClass('fa-chevron-circle-up');
+  					$a.find('i').toggleClass('fa-chevron-circle-down').toggleClass('fa-chevron-circle-up');
   					return false;
   				});
   			});
